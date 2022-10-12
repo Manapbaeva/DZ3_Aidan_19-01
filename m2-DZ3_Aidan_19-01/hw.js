57669708cfccd11560446b23247fd8e694736dbb
Manapbaeva <115273434+Manapbaeva@users.noreply.github.com>
2022-10-12 09:47:09 +0600

    Add files via upload

---
+++ m2-DZ3_Aidan_19-01/hw.js
@@ -0,0 +1,18 @@
+//Задача 1
+let a;
+if (a > 0){
+    let ggg = function () {
+        console.log('!');
+    }
+} else {
+    let ggg = function () {
+        console.log('!!')
+    }
+}
+
+//Задача 2
+let arr = [];
+for (i = 1; i <= 10; i++) {
+    arr[i] = 'x'
+}
+console.log(arr)
